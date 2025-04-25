6fd2a6142b359197250ce6775f9a4ddcfb51bc58
Erika <erikamc99.com>
2025-04-26 00:01:44 +0200

    feat: login and register dtos and user model

---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,8 @@
+namespace Muuki.DTOs
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Muuki.Models
+{
+    public class User
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("username")]
+        public string Username { get; set; }
+
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("passwordHash")]
+        public string PasswordHash { get; set; }
+    }
+}
